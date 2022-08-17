3d9b7c4b4dbd30f43a56e8cbf2678cfb9f5374c9
cyber_wolf <79078682+karthi08keyan@users.noreply.github.com>
2022-08-17 09:42:01 +0530

    Update student_table.js

--- student_table.js
+++ student_table.js
@@ -1,5 +1,5 @@
 <html lang="en">
-<head>
+<head> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
